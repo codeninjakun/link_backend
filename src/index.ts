b3a48edb16cd932d2149e02c7240d611ee8ba4c2
Rutuj-Runwal <rutujrunwal001@gmail.com>
2024-06-26 19:51:28 +0530

    Feat: User route configuration

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 // Server entry point
 
 import express from 'express';
+import { createNewUser, signin } from './routes/user';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import 'dotenv/config.js';
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Test!');
 });
+
+app.post('/signup', createNewUser);
+app.post('/signin', signin);
 
 app.listen(5000, () => {
   console.log('Works!');
